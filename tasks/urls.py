20e5c76a3235a5be581b7ea75d3ee43ce4786091
guidiamond <guilherme.batista770@gmail.com>
2020-12-02 04:34:23 -0300

    updated all routes

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,6 +8,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("get_all", views.get_tasks, name="get all tasks"),
-    path("deleteTasks", views.delete_tasks, name="delete all tasks"),
-    path("postTask", views.post_task, name="create new task"),
+    path("delete_all", views.delete_tasks, name="delete all tasks"),
+    path("create", views.post_task, name="create new task"),
 ]
