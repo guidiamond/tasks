6e1858a77aaae867a56583b89cd951a586d6973f
guidiamond <guilherme.batista770@gmail.com>
2020-12-02 04:16:38 -0300

    Removed coments and updated route

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,32 +1,13 @@
 from django.urls import path
 
-# import logger
 from django.http import HttpResponseNotAllowed
 
 from . import views
 
 
-# extracted from: https://stackoverflow.com/questions/19096227/how-to-discriminate-based-on-http-method-in-django-urlpatterns
-# def method_dispatch(**table):
-#     def invalid_method(request, *args, **kwargs):
-#         logger.warning(
-#             "Method Not Allowed (%s): %s",
-#             request.method,
-#             request.path,
-#             extra={"status_code": 405, "request": request},
-#         )
-#         return HttpResponseNotAllowed(table.keys())
-#
-#     def d(request, *args, **kwargs):
-#         handler = table.get(request.method, invalid_method)
-#         return handler(request, *args, **kwargs)
-#
-#     return d
-
-
 urlpatterns = [
     path("", views.index, name="index"),
-    path("getTasks", views.get_tasks, name="get all tasks"),
+    path("get_all", views.get_tasks, name="get all tasks"),
     path("deleteTasks", views.delete_tasks, name="delete all tasks"),
     path("postTask", views.post_task, name="create new task"),
 ]
